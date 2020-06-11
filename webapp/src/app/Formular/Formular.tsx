fda10cbd1ca4a25803490285ebbad8981f244fb9
admin <gautric@redhat.com>
2020-06-12 00:07:14 +0200

    cleanUp and add card component

--- webapp/src/app/Formular/Formular.tsx
+++ webapp/src/app/Formular/Formular.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import 'whatwg-fetch'
 import { Form,
+  Card, CardTitle, CardBody,
   FormGroup,
   TextInput,
   FormSelect,
@@ -58,15 +59,11 @@ class KYCForm extends React.Component<{},IKYCState> {
 
   handleSubmit(event) {
 
-    var httpHeaders = new Headers();
-    httpHeaders.append("Content-Type", "application/json");
-
     fetch(this.state.url, {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
-
       },
       body: JSON.stringify({
         "PEP": this.state.pep,
@@ -99,7 +96,6 @@ class KYCForm extends React.Component<{},IKYCState> {
     var ret = AlertVariant.default;
 
     if(level[name]){
-      console.log(level[name]);
       switch (level[name]) {
         case "LOW":
           ret = AlertVariant.success;
@@ -115,22 +111,6 @@ class KYCForm extends React.Component<{},IKYCState> {
     }
     return ret;
   }
-
-  convertKYC = (kyc) => {
-    var ret = AlertVariant.default;
-    
-    if(kyc){
-      if(kyc < 60) {
-        ret = AlertVariant.success;
-      } else if(kyc >= 60 && kyc <= 150){
-          ret = AlertVariant.warning;
-      } else {
-          ret = AlertVariant.danger;
-      }
-    }
-    return ret;
-  }
-
 
   handlePep = (checked, event) => {
     this.setState({ ["pep"] : event.target.checked });
@@ -156,21 +136,21 @@ class KYCForm extends React.Component<{},IKYCState> {
         <FormGroup
           label="URL of Kogito"
           isRequired
-          fieldId="simple-form-url">
+          fieldId="url-param">
           <TextInput
             isRequired
             type="text"
-            id="simple-form-url"
-            name="simple-form-url"
-            aria-describedby="simple-form-url-helper"
+            id="url-param"
+            name="url-param"
+            aria-describedby="url-param-helper"
             value={this.state.url}
             onChange={this.handleUrl}
           />
         </FormGroup>
         <FormGroup
           label="Political Exposed Person"
-          fieldId="pep">
-          <Switch id="pep" 
+          fieldId="pep-param">
+          <Switch id="pep-param" 
             label="Political Exposed Person"
             labelOff="Anonymous Person"
             isChecked={this.state.pep}
@@ -180,11 +160,11 @@ class KYCForm extends React.Component<{},IKYCState> {
         <FormGroup
           label="Amount"
           isRequired
-          fieldId="amount">
+          fieldId="amount-param">
           <TextInput
             isRequired
             type="text"
-            id="amount"
+            id="amount-param"
             name="amount"
             aria-describedby="amount"
             value={this.state.amount}
@@ -195,8 +175,8 @@ class KYCForm extends React.Component<{},IKYCState> {
         <FormGroup
           label="Fiscal Residency"
           isRequired
-          fieldId="fiscalResidency">
-          <FormSelect value={this.state.fiscalResidency} onChange={this.handleFiscalResidency} aria-label="FormSelect Input">
+          fieldId="fiscalResidency-param">
+          <FormSelect id="fiscalResidency-param" value={this.state.fiscalResidency} onChange={this.handleFiscalResidency} aria-label="FormSelect Input">
             {this.options.map((option, index) => (
               <FormSelectOption isDisabled={option.disabled} key={index} value={option.value} label={option.label} />
             ))}
@@ -206,7 +186,6 @@ class KYCForm extends React.Component<{},IKYCState> {
         <ActionGroup>
           <Button variant="primary" type="submit">Envoyer</Button>
         </ActionGroup>
-
 
         <Modal
           title="KYC DMN Result"
@@ -219,10 +198,10 @@ class KYCForm extends React.Component<{},IKYCState> {
             </Button>
           ]}
         >          
-          <Alert variant={this.convertLevel(this.state.result,"PEP Rule")} title={"PEP Score " + this.state.result["PEP Rule"]} /> 
-          <Alert variant={this.convertLevel(this.state.result,"Amount Rule")} title={"Amount Score " + this.state.result["Amount Rule"]} /> 
-          <Alert variant={this.convertLevel(this.state.result,"Fiscal Residency Rule")} title={"Fiscal Residency Score " + this.state.result["Fiscal Residency Rule"]} /> 
-          <Alert variant={this.convertLevel(this.state.result.KYC,"Level")}  title={"Score KYC " + this.state.result.KYC.Score} />  
+          <Alert variant={this.convertLevel(this.state.result,"PEP Rule")} isInline title={"PEP Score " + this.state.result["PEP Rule"]} /> 
+          <Alert variant={this.convertLevel(this.state.result,"Amount Rule")} isInline title={"Amount Score " + this.state.result["Amount Rule"]} /> 
+          <Alert variant={this.convertLevel(this.state.result,"Fiscal Residency Rule")} isInline title={"Fiscal Residency Score " + this.state.result["Fiscal Residency Rule"]} /> 
+          <Alert variant={this.convertLevel(this.state.result.KYC,"Level")} isInline title={"Score KYC " + this.state.result.KYC.Score} />  
         </Modal>
       </Form>
     );
@@ -232,8 +211,14 @@ class KYCForm extends React.Component<{},IKYCState> {
 
 const Formular: React.FunctionComponent = () => (
   <PageSection>
-    <Title headingLevel="h1" size="lg">KYC Formular</Title>
-    <KYCForm/>
+    <Card>
+      <CardTitle>  
+        <Title headingLevel="h1" size="lg">KYC Formular</Title>
+      </CardTitle>
+      <CardBody className="kyc"> 
+        <KYCForm/>
+      </CardBody>
+    </Card>
   </PageSection>
 )
 
