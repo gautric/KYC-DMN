0bead11b16e271ab3cc139d2f848f5fdac0dd87f
admin <gautric@redhat.com>
2020-10-02 20:56:30 +0200

    Externalize keycloak config

--- webapp/src/app/keycloak.ts
+++ webapp/src/app/keycloak.ts
@@ -1,11 +1,15 @@
 import Keycloak from 'keycloak-js';
 
+const AUTH_URL= process.env.AUTH_URL || "http://localhost/auth";
+const AUTH_REALM=process.env.AUTH_REALM ||  "kyc-realm" ; 
+const AUTH_CLIENTID=process.env.AUTH_CLIENTID ||  "kyc-clientId";
+
 // Setup Keycloak instance as needed
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
 const keycloak = Keycloak({
-  url: 'http://localhost:8080/auth',
-  realm: 'example',
-  clientId: 'kyc-dmn',
+  url: AUTH_URL,
+  realm: AUTH_REALM,
+  clientId: AUTH_CLIENTID,
 });
 
 export default keycloak;
