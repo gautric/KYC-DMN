c0cba4cf6ccd2a80289c00f199b06bf8c245c590
admin <gautric@redhat.com>
2022-01-06 02:16:21 +0100

    add some prop for context

--- webapp/src/app/KYCContext.tsx
+++ webapp/src/app/KYCContext.tsx
@@ -2,5 +2,10 @@ import * as React from 'react';
 
 export const KYC_DMN_URL = process.env.KYC_DMN_URL;
 
-export const KYCContext = React.createContext({url: KYC_DMN_URL});
+export const KYCContext = React.createContext({
+    url: process.env.KYC_DMN_URL,
+    apiUrl : process.env.KYC_DMN_API_URL,
+    metricsUrl : process.env.KYC_DMN_METRICS_URL,
+    
+    });
 KYCContext.displayName = 'KYCContext';
