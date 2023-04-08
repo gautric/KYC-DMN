cf0386c4af726f0bca5d38af2b6565ee098a5092
gautric <gregoire@redhat.com>
2023-04-08 23:57:12 +0200

    update

--- webapp/src/app/Config/Config.tsx
+++ webapp/src/app/Config/Config.tsx
@@ -1,5 +1,5 @@
 //import * as React from 'react';
-import React, {useState} from "react";
+import React from "react";
 import 'whatwg-fetch';
 import { Card,
   CardTitle,
@@ -13,82 +13,37 @@ import { Card,
   DataListCell,
   Title } from '@patternfly/react-core';
 
-import { KYCContext } from '@app/KYCContext';
+import { useContextStore } from '@app/Context';
 
-export interface URLInputProps extends Omit<React.HTMLProps<HTMLInputElement>, 'onChange' | 'ref'> {
+const Config: React.FunctionComponent = () => {
 
-    /** A reference object to attach to the input box. */
-    innerRef?: React.RefObject<any>;
+  const {apiURL, updateAPIURL} = useContextStore();
 
-    /** A callback for when the input value changes. */
-    onChange?: (value: string, event: React.FormEvent<HTMLInputElement>) => void;
-
-    /** Value of the input. */
-    value?: string ;
-
+  return (
+    <PageSection>
+      <Card>
+        <CardTitle>
+          <Title headingLevel="h1" size="lg">KYC Config</Title>
+        </CardTitle>
+        <CardBody>
+          <>
+            <DataList aria-label="Simple data list example">
+              <DataListItem aria-labelledby="header">
+                <DataListItemRow>
+                  <DataListItemCells 
+                    dataListCells={[
+                      <DataListCell key="URL Field">URL</DataListCell>,
+                      <DataListCell key="URL Value"><TextInput value={apiURL} type="url" onChange={updateAPIURL} aria-label="text input example" /> </DataListCell>
+                    ]}
+                  />
+                </DataListItemRow>
+              </DataListItem>
+            </DataList>
+          </>
+        </CardBody>
+      </Card>
+    </PageSection>
+  )
 }
-
-export class  URLInputBase extends React.Component<URLInputProps>  {
-    static displayName = 'URLInputBase';
-    static contextType = KYCContext;
-    static defaultProps: URLInputProps = {
-    onChange: (): any => undefined
-    };
-    inputRef = React.createRef<HTMLInputElement>();
-
-    render() {
-        const {
-            value,
-            /* eslint-disable @typescript-eslint/no-unused-vars */
-            onChange,
-            ...props
-        } = this.props;
-        return (
-            <TextInput value={this.context.apiUrl} type="url" isReadOnly aria-label="text input example" />
-        );
-    }
-}
-
-export const URLInput = React.forwardRef((props: URLInputProps, ref: React.Ref<HTMLInputElement>) => (
-    <URLInputBase {...props} innerRef={ref as React.MutableRefObject<any>} />
-));
-
-
-// class Child extends React.Component {
-//   render() {
-//     const renderedContext = this.context;
-//     console.log({ this: this });
-//     console.log(renderedContext);
-//     console.log({ asd: Child.contextType });
-//     return <div />;
-//   }
-// }
-// Child.contextType = KYCContext;
-
-const Config: React.FunctionComponent = () => (
-  <PageSection>
-    <Card>
-      <CardTitle>
-        <Title headingLevel="h1" size="lg">KYC Config</Title>
-      </CardTitle>
-      <CardBody>
-        <>
-          <DataList aria-label="Simple data list example">
-            <DataListItem aria-labelledby="header">
-              <DataListItemRow>
-                <DataListItemCells 
-                  dataListCells={[
-                    <DataListCell key="URL Field">URL</DataListCell>,
-                    <DataListCell key="URL Value"><URLInput onChange={(e) => console.log(e)}/> </DataListCell>
-                  ]}
-                />
-              </DataListItemRow>
-            </DataListItem>
-          </DataList>
-        </>
-      </CardBody>
-    </Card>
-  </PageSection>
-)
 
 export { Config };
